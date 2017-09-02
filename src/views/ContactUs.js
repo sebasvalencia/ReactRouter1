3e73efe2fbb835925540aa17bc0ca38f4c19c0b3
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-09-02 17:48:20 -0500

    Creando routers

---
+++ src/views/ContactUs.js
@@ -0,0 +1,17 @@
+import React , {Component} from 'react';
+
+class ContactUs extends Component{
+
+
+  render(){
+    return(
+      <div>
+        <h2>ContactUs</h2>
+      </div>
+    );
+  }
+
+
+}
+
+export default ContactUs;
