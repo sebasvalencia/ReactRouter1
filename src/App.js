7585ca9763bbff67b7151e61f6c99098653daa59
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-09-02 17:48:08 -0500

    Creando routers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './views/Home';
+import ContactUs from './views/ContactUs'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <Route  exact path="/" component={Home}/>
+          <Route path="/contact-us" component={ContactUs} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
+
     );
   }
 }
